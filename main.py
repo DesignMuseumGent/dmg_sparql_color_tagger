d6dcbcdac672a71bad577ef4866d454e1f2f0065
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-07-07 16:24:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from src.utils.sparql_harvester import sparql_harvester
 from crontab import CronTab
 def main():
     ## 1. run harvester (weekly)
-    #sparql_harvester() #todo: make that it only puts in new versions if already existent
+    sparql_harvester() #todo: make that it only puts in new versions if already existent
     ## 2.  fetch colors + fetch licenses and attributions.;
     populate_database() #todo: only check for new items. Don't rerun whole thing.
     ## 3. parse colors:
-    #parse_colors()
+    parse_colors()
 
 main()
 
